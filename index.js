272faf99a7eab62e8abe31115df535f7c82c228a
odera <eugeneodera59@gmail.com>
2024-10-10 18:42:54 +0300

    add text

--- index.js
+++ index.js
@@ -1,1 +1,47 @@
 // Add your code here
+
+// function submitData(name, email) { 
+//     let name = "eugene"
+//     let Email = "eugeneodera59@gmail.com"
+// }
+// console.log(submitData(name, email));
+
+// fetch("http://localhost:3000/users", configurationObject);
+// const configurationObject = {
+//     method: "POST",
+//     headers: {
+//         "Content-Type": "application/json",
+//         "Accept": "application/json",
+//       },
+//       body: JSON.stringify({
+//         userName: "eugene",
+//         userEmail:"eugeneodera59@gmail.com",
+//       })
+      
+//   };
+
+function submitData(name, email) {
+return fetch("http://localhost:3000/users",{
+    method: 'POST',
+    headers: {
+    "Content-Type": "application/json",
+    "Accept": "application/json",
+    },
+    body: JSON.stringify({
+        name, 
+        email
+    })
+    
+})
+
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (object) {
+    document.body.innerHTML = object[ "id" ]
+  })
+   .catch(function (error){
+    document.body.innerHTML = error.message
+   })
+  
+}
